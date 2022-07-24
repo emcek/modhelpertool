aaf17fe1fd8127d14469497d62e3a28814412f1f
Michał Plichta <mplichta@gmail.com>
2022-07-24 23:50:24 +0200

    optimize imports

--- mht/utils.py
+++ mht/utils.py
@@ -1,4 +1,4 @@
-import re
+from re import search, MULTILINE
 from typing import Tuple
 
 
@@ -12,14 +12,14 @@ def parse_cleaning(out: str, err: str, mod_filename: str) -> Tuple[bool, str]:  
     :return: Result and reason
     """
     ceases = {
-        1: {'args': (r'^\[ERROR \({}\): Master: (.* not found) in <DATADIR>]$'.format(mod_filename), err, re.MULTILINE),
+        1: {'args': (r'^\[ERROR \({}\): Master: (.* not found) in <DATADIR>]$'.format(mod_filename), err, MULTILINE),
             'result': False},
-        2: {'args': (r'^{} was (not modified)$'.format(mod_filename), out, re.MULTILINE),
+        2: {'args': (r'^{} was (not modified)$'.format(mod_filename), out, MULTILINE),
             'result': False},
-        3: {'args': (r'Output (saved) in: "1/{}"\nOriginal unaltered: "{}"'.format(mod_filename, mod_filename), out, re.MULTILINE),
+        3: {'args': (r'Output (saved) in: "1/{}"\nOriginal unaltered: "{}"'.format(mod_filename, mod_filename), out, MULTILINE),
             'result': True},
     }
     for data in ceases.values():
-        match = re.search(*data['args'])  # type: ignore
+        match = search(*data['args'])  # type: ignore
         if match:
             return data['result'], match.group(1)  # type: ignore
