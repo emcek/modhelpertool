9959f490aa380168c904eed9ce7281eb8a310cd2
Michał Plichta <mplichta@gmail.com>
2022-07-25 00:49:53 +0200

    configure logger

--- mht/__init__.py
+++ mht/__init__.py
@@ -3,7 +3,10 @@ from os import name
 from platform import architecture, uname, python_implementation, python_version
 from sys import platform
 
+from mht.log import config_logger
+
 LOG = getLogger(__name__)
+config_logger(logger=LOG, verbose=True)
 LOG.debug(f'Arch: {name} / {platform} / {" / ".join(architecture())}')
 LOG.debug(f'Python: {python_implementation()}-{python_version()}')
 LOG.debug(f'{uname()}')
