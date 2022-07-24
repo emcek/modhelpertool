cab2107db98c33d4eda7e1e71fd744bb9b042768
Michał Plichta <mplichta@gmail.com>
2022-07-24 23:00:44 +0200

    add 3 simple tests

---
+++ tests/test_tk_gui.py
@@ -0,0 +1,45 @@
+from mht.tk_gui import parse_cleaning
+
+
+def test_parse_cleaning_success():
+    err = ""
+    out = """CLEANING: "FLG - Balmora's Underworld V1.1.esp" ...
+Loaded cached Master: <DATADIR>/morrowind.esm
+Loaded cached Master: <DATADIR>/tribunal.esm
+Loaded cached Master: <DATADIR>/bloodmoon.esm
+ Cleaned duplicate record (STAT): in_dwrv_corr3_02
+ Cleaned redundant AMBI,WHGT from CELL: omaren ancestral tomb
+Output saved in: "1/FLG - Balmora's Underworld V1.1.esp"
+Original unaltered: "FLG - Balmora's Underworld V1.1.esp"
+
+Cleaning Stats for "FLG - Balmora's Underworld V1.1.esp":
+                duplicate record:     1
+             redundant CELL.AMBI:     1
+             redundant CELL.WHGT:     1"""
+    assert parse_cleaning(out, err, "FLG - Balmora's Underworld V1.1.esp") == (True, '')
+
+
+def test_parse_cleaning_not_modified():
+    err = ""
+    out = """CLEANING: "FLG - Balmora's Underworld V1.1.esp" ...
+Loaded cached Master: <DATADIR>/morrowind.esm
+Loaded cached Master: <DATADIR>/tribunal.esm
+Loaded cached Master: <DATADIR>/bloodmoon.esm
+FLG - Balmora's Underworld V1.1.esp was not modified"""
+    assert parse_cleaning(out, err, "FLG - Balmora's Underworld V1.1.esp") == (False, 'Not modified')
+
+
+def test_parse_cleaning_no_master():
+    err = """Use of uninitialized value in -s at /home/emc/git/Modding-OpenMW/modhelpertool/mht/tes3cmd-0.37w line 6282.
+Use of uninitialized value $curr_size in numeric eq (==) at /home/emc/git/Modding-OpenMW/modhelpertool/mht/tes3cmd-0.37w line 6283.
+Use of uninitialized value $curr_size in concatenation (.) or string at /home/emc/git/Modding-OpenMW/modhelpertool/mht/tes3cmd-0.37w line 6287.
+Cache Invalidated for: oaab_data.esm (curr_size == , prev_size == 1269934) at /home/emc/git/Modding-OpenMW/modhelpertool/mht/tes3cmd-0.37w line 6287.
+
+[ERROR (Caldera.esp): Master: oaab_data.esm not found in <DATADIR>]"""
+    out = """CLEANING: "Caldera.esp" ...
+Loaded cached Master: <DATADIR>/morrowind.esm
+Loaded cached Master: <DATADIR>/tribunal.esm
+Loaded cached Master: <DATADIR>/bloodmoon.esm
+Loading Master: oaab_data.esm
+Caldera.esp was not modified"""
+    assert parse_cleaning(out, err, 'Caldera.esp') == (False, 'No master: oaab_data.esm')
