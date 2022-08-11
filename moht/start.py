9ab2cf256197fbc7b379fb457c49462ed3135f08
Michał Plichta <mplichta@gmail.com>
2022-08-11 22:16:07 +0200

    increase height on main window

--- moht/start.py
+++ moht/start.py
@@ -7,7 +7,7 @@ from moht.tk_gui import MohtTkGui
 def run():
     """Function to start Mod Helper Tool GUI."""
     root = tk.Tk()
-    width, height = 500, 200
+    width, height = 500, 230
     root.title('Mod Helper Tool')
     root.geometry(f'{width}x{height}')
     root.minsize(width=width, height=height)
