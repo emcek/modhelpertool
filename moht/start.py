30ea6f843c4a11279ea1e5eb132c603a1d8d8fb9
Michał Plichta <mplichta@gmail.com>
2022-08-12 23:42:04 +0200

    add cli parser

--- moht/start.py
+++ moht/start.py
@@ -1,17 +1,17 @@
 import signal
 import sys
 import tkinter as tk
+from argparse import ArgumentParser, RawTextHelpFormatter
 from os import path
 
 from PyQt5 import QtCore
 from PyQt5.QtCore import QLibraryInfo, QTranslator, QLocale
 from PyQt5.QtWidgets import QApplication
 
-from moht.tkgui import MohtTkGui
-from moht.qtgui import MohtQtGui
+from moht import VERSION, tkgui, qtgui
 
 
-def run():
+def run_tk():
     """Function to start Mod Helper Tool Tk GUI."""
     root = tk.Tk()
     width, height = 500, 230
@@ -19,11 +19,11 @@ def run():
     root.geometry(f'{width}x{height}')
     root.minsize(width=width, height=height)
     root.iconphoto(False, tk.PhotoImage(file=path.join(path.abspath(path.dirname(__file__)), 'img', 'moht.png')))
-    gui = MohtTkGui(master=root)
+    gui = tkgui.MohtTkGui(master=root)
     gui.mainloop()
 
 
-def run_qtgui():
+def run_qt():
     """Function to start Mod Helper Tool QtGUI."""
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
     signal.signal(signal.SIGTERM, signal.SIG_DFL)
@@ -37,10 +37,22 @@ def run_qtgui():
     if translator.load(QLocale.system(), 'qtgui', '-', path.abspath(path.dirname(__file__))):  # change to _
         app.installTranslator(translator)
 
-    window = MohtQtGui()
+    window = qtgui.MohtQtGui()
     window.show()
     sys.exit(app.exec())
 
 
 if __name__ == '__main__':
-    run_qtgui()
+    parser = ArgumentParser(description='desc', formatter_class=RawTextHelpFormatter)
+    parser.add_argument('-V', '--version', action='version', version='%(prog)s Version: ' + VERSION)
+    gui = parser.add_subparsers(title='gui', dest='gui', description='Available subcommands', help='Choose one of GUI')
+    gui_qt = gui.add_parser(name='qt', help='Starting Qt5 GUI interface for Moht', formatter_class=RawTextHelpFormatter)
+    gui_qt.add_argument('-style', dest='style', help='Style for QtGUI: "fusion" (default) or "windows".', default='fusion')
+    gui_tk = gui.add_parser(name='tk', help='Starting Tk GUI interface for Moht', formatter_class=RawTextHelpFormatter)
+    args = parser.parse_args()
+    if args.gui == 'qt':
+        run_qt()
+    elif args.gui == 'tk':
+        run_tk()
+    else:
+        parser.print_help()
