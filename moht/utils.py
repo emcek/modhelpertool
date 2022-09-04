31f307f847e4e6ddd9c80a07f6c54618b3d138c7
Michał Plichta <mplichta@gmail.com>
2022-09-04 23:16:47 +0200

    fix tests

--- moht/utils.py
+++ moht/utils.py
@@ -73,6 +73,7 @@ def is_latest_ver(package: str, current_ver: str) -> Tuple[bool, str]:
     """
     remote_ver = current_ver
     extra_data = 'No updates'
+    problem = False
     out, err = run_cmd(f'pip install --dry-run --no-color --timeout 3 --retries 1 --progress-bar off --upgrade {package}')
     match = search(r'Would install\s.*{}-([\d.-]+)'.format(package), out)
     if match:
@@ -81,8 +82,9 @@ def is_latest_ver(package: str, current_ver: str) -> Tuple[bool, str]:
         extra_data = f'Update available: {remote_ver}'
     match = search(r'no such option:\s(.*)', err)
     if match:
-        extra_data = match.group(1)
-        logger.warning(f'Version check failed, unknown switch: {extra_data}')
+        problem = True
+        extra_data = f'Version check failed, unknown switch: {match.group(1)}'
+        logger.warning(extra_data)
         out, _ = run_cmd('pip list')
         match = search(r'pip\s*([\d.]*)', out)
         if match:
@@ -90,7 +92,7 @@ def is_latest_ver(package: str, current_ver: str) -> Tuple[bool, str]:
             extra_data = f'Version check failed, old pip: {pip_ver}'
             logger.debug(f'Pip version: {pip_ver}')
     latest = _compare_versions(package, current_ver, remote_ver)
-    if latest and current_ver == remote_ver:
+    if latest and current_ver == remote_ver and not problem:
         extra_data = 'No updates'
     return latest, extra_data
 
