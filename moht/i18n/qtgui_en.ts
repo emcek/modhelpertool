c65e678482d9db7fef0430152f6526b9b022c671
Michał Plichta <mplichta@gmail.com>
2022-09-05 21:29:31 +0200

    add i18n files

---
+++ moht/i18n/qtgui_en.ts
@@ -0,0 +1,291 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../ui/about.ui" line="32"/>
+        <source>About</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="../ui/about.ui" line="65"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Mod Helper Tool (Moht)&lt;/span&gt;&lt;br/&gt;&lt;br/&gt;
+Author: Michal Plichta: &lt;a href=&quot;https://gitlab.com/emcek&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Gitlab&lt;/span&gt;&lt;/a&gt; and &lt;a href=&quot;https://github.com/emcek&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Github&lt;/span&gt;&lt;/a&gt;&lt;br/&gt;&lt;br/&gt;
+Project website: &lt;a href=&quot;https://gitlab.com/modding-openmw/modhelpertool/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Modding-OpenMW/ModHelperTool&lt;/span&gt;&lt;/a&gt;&lt;br/&gt;
+Discord: &lt;a href=&quot;https://discord.gg/sKyZwumQuU&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;OpenMW/modding-openmw-dot-com&lt;/span&gt;&lt;/a&gt;&lt;br/&gt;
+Report isssue: &lt;a href=&quot;https://gitlab.com/modding-openmw/modhelpertool/issues&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Gitlab issues&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Mod Helper Tool (Moht)&lt;/span&gt;&lt;br/&gt;&lt;br/&gt;
+Author: Michal Plichta: &lt;a href=&quot;https://gitlab.com/emcek&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Gitlab&lt;/span&gt;&lt;/a&gt; and &lt;a href=&quot;https://github.com/emcek&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Github&lt;/span&gt;&lt;/a&gt;&lt;br/&gt;&lt;br/&gt;
+Project website: &lt;a href=&quot;https://gitlab.com/modding-openmw/modhelpertool/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Modding-OpenMW/ModHelperTool&lt;/span&gt;&lt;/a&gt;&lt;br/&gt;
+Discord: &lt;a href=&quot;https://discord.gg/sKyZwumQuU&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;OpenMW/modding-openmw-dot-com&lt;/span&gt;&lt;/a&gt;&lt;br/&gt;
+Report isssue: &lt;a href=&quot;https://gitlab.com/modding-openmw/modhelpertool/issues&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Gitlab issues&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>MohtQtGui</name>
+    <message>
+        <location filename="../qtgui.py" line="105"/>
+        <source>Double click on item to copy plugin`s path.</source>
+        <translation>Double click on item to copy plugin`s path.</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="106"/>
+        <source>Cleaning time in min:sec
+Hold on item to see cleaning details.</source>
+        <translation>Cleaning time in min:sec
+Hold on item to see cleaning details.</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="185"/>
+        <source>Done. Took: {duration}</source>
+        <translation>Done. Took: {duration}</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="220"/>
+        <source>Path of plugin copied to clipboard</source>
+        <translation>Path of plugin copied to clipboard</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="224"/>
+        <source>ver. {VERSION} - {desc}</source>
+        <translation>ver. {VERSION} - {desc}</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="97"/>
+        <source>Cleaned: 0</source>
+        <translation>Cleaned: 0</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="98"/>
+        <source>Error: 0</source>
+        <translation>Error: 0</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="99"/>
+        <source>Clean: 0</source>
+        <translation>Clean: 0</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="285"/>
+        <source>
+Check for `perl-Config-IniFiles` or a similar package.
+Use you package manage:
+
+Arch / Manjaro (AUR):
+yay -S perl-config-inifiles
+
+Gentoo:
+emerge dev-perl/Config-IniFiles
+
+Debian / Ubuntu / Mint:
+apt install libconfig-inifiles-perl
+
+OpenSUSE:
+zypper install perl-Config-IniFiles
+
+Fedora / CentOS / RHEL:
+dnf install perl-Config-IniFiles.noarch</source>
+        <translation>
+Check for `perl-Config-IniFiles` or a similar package.
+Use you package manage:
+
+Arch / Manjaro (AUR):
+yay -S perl-config-inifiles
+
+Gentoo:
+emerge dev-perl/Config-IniFiles
+
+Debian / Ubuntu / Mint:
+apt install libconfig-inifiles-perl
+
+OpenSUSE:
+zypper install perl-Config-IniFiles
+
+Fedora / CentOS / RHEL:
+dnf install perl-Config-IniFiles.noarch</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="304"/>
+        <source>Selected file is not a valid tes3cmd executable.
+
+Please select a correct binary file.</source>
+        <translation>Selected file is not a valid tes3cmd executable.
+
+Please select a correct binary file.</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="124"/>
+        <source>Plugins to clean: {self.no_of_plugins} - See Report</source>
+        <translation>Plugins to clean: {self.no_of_plugins} - See Report</translation>
+    </message>
+</context>
+<context>
+    <name>QtGui</name>
+    <message>
+        <location filename="../ui/qtgui.ui" line="20"/>
+        <source>Mod Helper Tool</source>
+        <translation>Mod Helper Tool</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="38"/>
+        <source>Close</source>
+        <translation>Close</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="45"/>
+        <source>Check Updates</source>
+        <translation>Check Updates</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="56"/>
+        <source>Cleaner</source>
+        <translation>Cleaner</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="79"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select &lt;span style=&quot; font-weight:600;&quot;&gt;Data Files &lt;/span&gt;folder of Morrowind installation&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select &lt;span style=&quot; font-weight:600;&quot;&gt;Data Files &lt;/span&gt;folder of Morrowind installation&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="82"/>
+        <source>Select Data Files</source>
+        <translation>Select Data Files</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="89"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select root folder with Mods to clean.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select root folder with Mods to clean.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="92"/>
+        <source>Select Mods</source>
+        <translation>Select Mods</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="105"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select custom &lt;span style=&quot; font-weight:600;&quot;&gt;tes3cmd&lt;/span&gt; executable file, or keep using built-in one&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select custom &lt;span style=&quot; font-weight:600;&quot;&gt;tes3cmd&lt;/span&gt; executable file, or keep using built-in one&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="108"/>
+        <source>Select tes3cmd</source>
+        <translation>Select tes3cmd</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="115"/>
+        <source>Clean</source>
+        <translation>Clean</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="138"/>
+        <source>Report</source>
+        <translation>Raport</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="151"/>
+        <source>built-in v0.40</source>
+        <translation>built-in v0.40</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="161"/>
+        <source>built-in v0.37</source>
+        <translation>built-in v0.37</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="171"/>
+        <source>custom</source>
+        <translation>custom</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="181"/>
+        <source>After successful clean-up:</source>
+        <translation>After successful clean-up:</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="190"/>
+        <source>Remove backups of plugins</source>
+        <translation>Remove backups of plugins</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="200"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Clean-up proccess create cache of master files in Data FIles folder.&lt;/p&gt;&lt;p&gt;It speed up cleaning a bit, but it takes some disk space as well.&lt;/p&gt;&lt;p&gt;When checkbox is checked, this cache is removed.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Clean-up proccess create cache of master files in Data FIles folder.&lt;/p&gt;&lt;p&gt;It speed up cleaning a bit, but it takes some disk space as well.&lt;/p&gt;&lt;p&gt;When checkbox is checked, this cache is removed.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="203"/>
+        <source>Remove cache of master files</source>
+        <translation>Remove cache of master files</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="234"/>
+        <source>Plugin</source>
+        <translation>Plugin</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="239"/>
+        <source>Status</source>
+        <translation>Status</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="244"/>
+        <source>Time</source>
+        <translation>Time</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="252"/>
+        <source>Back</source>
+        <translation>Back</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="277"/>
+        <source>File</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="283"/>
+        <source>Help</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="298"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Quit</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="301"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="306"/>
+        <source>Report issue</source>
+        <translation>Report issue</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="311"/>
+        <source>About Moht</source>
+        <translation>About Moht</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="316"/>
+        <source>About Qt</source>
+        <translation>About Qt</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="321"/>
+        <source>Check updates</source>
+        <translation>Check updates</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="187"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;During clean-up plugin file is copied to Data Files folder.&lt;/p&gt;&lt;p&gt;Cleaning proces create new plugin file.&lt;/p&gt;&lt;p&gt;When checkbox is checked dirty plugin file is removed.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;During clean-up plugin file is copied to Data Files folder.&lt;/p&gt;&lt;p&gt;Cleaning proces create new plugin file.&lt;/p&gt;&lt;p&gt;When checkbox is checked dirty plugin file is removed.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="145"/>
+        <source>Version of tes3cmd:</source>
+        <translation>Version of tes3cmd:</translation>
+    </message>
+</context>
+</TS>
---
+++ moht/i18n/qtgui_pl.ts
@@ -0,0 +1,291 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../ui/about.ui" line="32"/>
+        <source>About</source>
+        <translation>O</translation>
+    </message>
+    <message>
+        <location filename="../ui/about.ui" line="65"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Mod Helper Tool (Moht)&lt;/span&gt;&lt;br/&gt;&lt;br/&gt;
+Author: Michal Plichta: &lt;a href=&quot;https://gitlab.com/emcek&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Gitlab&lt;/span&gt;&lt;/a&gt; and &lt;a href=&quot;https://github.com/emcek&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Github&lt;/span&gt;&lt;/a&gt;&lt;br/&gt;&lt;br/&gt;
+Project website: &lt;a href=&quot;https://gitlab.com/modding-openmw/modhelpertool/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Modding-OpenMW/ModHelperTool&lt;/span&gt;&lt;/a&gt;&lt;br/&gt;
+Discord: &lt;a href=&quot;https://discord.gg/sKyZwumQuU&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;OpenMW/modding-openmw-dot-com&lt;/span&gt;&lt;/a&gt;&lt;br/&gt;
+Report isssue: &lt;a href=&quot;https://gitlab.com/modding-openmw/modhelpertool/issues&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Gitlab issues&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Mod Helper Tool (Moht)&lt;/span&gt;&lt;br/&gt;&lt;br/&gt;
+Autor: Michal Plichta: &lt;a href=&quot;https://gitlab.com/emcek&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Gitlab&lt;/span&gt;&lt;/a&gt; i &lt;a href=&quot;https://github.com/emcek&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Github&lt;/span&gt;&lt;/a&gt;&lt;br/&gt;&lt;br/&gt;
+Strona projektu: &lt;a href=&quot;https://gitlab.com/modding-openmw/modhelpertool/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Modding-OpenMW/ModHelperTool&lt;/span&gt;&lt;/a&gt;&lt;br/&gt;
+Discord: &lt;a href=&quot;https://discord.gg/sKyZwumQuU&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;OpenMW/modding-openmw-dot-com&lt;/span&gt;&lt;/a&gt;&lt;br/&gt;
+Załaszanie problemów: &lt;a href=&quot;https://gitlab.com/modding-openmw/modhelpertool/issues&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Zgłoszenia Gitlab&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>MohtQtGui</name>
+    <message>
+        <location filename="../qtgui.py" line="105"/>
+        <source>Double click on item to copy plugin`s path.</source>
+        <translation>Podwójne kliknięcie kopijuje ścieżkę do pluginu.</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="106"/>
+        <source>Cleaning time in min:sec
+Hold on item to see cleaning details.</source>
+        <translation>Czas czyszczenia w in min:sek
+Zatrzymaj nad, aby zobaczyć szczegóły.</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="185"/>
+        <source>Done. Took: {duration}</source>
+        <translation>Zrobione. Zajęło: {duration}</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="220"/>
+        <source>Path of plugin copied to clipboard</source>
+        <translation>Ścieżka do pluginu skopiowana do schowka</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="224"/>
+        <source>ver. {VERSION} - {desc}</source>
+        <translation>wersja {VERSION} - {desc}</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="97"/>
+        <source>Cleaned: 0</source>
+        <translation>Wyszyszczone: 0</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="98"/>
+        <source>Error: 0</source>
+        <translation>Błąd: 0</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="99"/>
+        <source>Clean: 0</source>
+        <translation>Czyste: 0</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="285"/>
+        <source>
+Check for `perl-Config-IniFiles` or a similar package.
+Use you package manage:
+
+Arch / Manjaro (AUR):
+yay -S perl-config-inifiles
+
+Gentoo:
+emerge dev-perl/Config-IniFiles
+
+Debian / Ubuntu / Mint:
+apt install libconfig-inifiles-perl
+
+OpenSUSE:
+zypper install perl-Config-IniFiles
+
+Fedora / CentOS / RHEL:
+dnf install perl-Config-IniFiles.noarch</source>
+        <translation>
+Sprawdż `perl-Config-IniFiles` lub podobną paczkę.
+Użyj swojego menadżera paczek:
+
+Arch / Manjaro (AUR):
+yay -S perl-config-inifiles
+
+Gentoo:
+emerge dev-perl/Config-IniFiles
+
+Debian / Ubuntu / Mint:
+apt install libconfig-inifiles-perl
+
+OpenSUSE:
+zypper install perl-Config-IniFiles
+
+Fedora / CentOS / RHEL:
+dnf install perl-Config-IniFiles.noarch</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="304"/>
+        <source>Selected file is not a valid tes3cmd executable.
+
+Please select a correct binary file.</source>
+        <translation>Wybrany plik nie jest wykonywalnym poleceniem tes3cmd.
+
+Prosze wybierz poprawny plik wykonywalny.</translation>
+    </message>
+    <message>
+        <location filename="../qtgui.py" line="124"/>
+        <source>Plugins to clean: {self.no_of_plugins} - See Report</source>
+        <translation>Pluginów do wyczyszczenia: {self.no_of_plugins} - Zobacz Report</translation>
+    </message>
+</context>
+<context>
+    <name>QtGui</name>
+    <message>
+        <location filename="../ui/qtgui.ui" line="20"/>
+        <source>Mod Helper Tool</source>
+        <translation>Mod Helper Tool</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="38"/>
+        <source>Close</source>
+        <translation>Zamknij</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="45"/>
+        <source>Check Updates</source>
+        <translation>Sprawdź aktualizacje</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="56"/>
+        <source>Cleaner</source>
+        <translation>Wyczyść</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="79"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select &lt;span style=&quot; font-weight:600;&quot;&gt;Data Files &lt;/span&gt;folder of Morrowind installation&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Wybierz &lt;span style=&quot; font-weight:600;&quot;&gt;Data Files &lt;/span&gt;folder z  instalacją Morrowind&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="82"/>
+        <source>Select Data Files</source>
+        <translation>Wybierz Data Files</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="89"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select root folder with Mods to clean.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Wybierz folder z Modami do wyczyszczenia.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="92"/>
+        <source>Select Mods</source>
+        <translation>Wybierz Mody</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="105"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select custom &lt;span style=&quot; font-weight:600;&quot;&gt;tes3cmd&lt;/span&gt; executable file, or keep using built-in one&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Wybierz własny &lt;span style=&quot; font-weight:600;&quot;&gt;tes3cmd&lt;/span&gt; plik wykonywalny, albo używaj wybudowanego&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="108"/>
+        <source>Select tes3cmd</source>
+        <translation>Wybierz tes3cmd</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="115"/>
+        <source>Clean</source>
+        <translation>Wyczyść</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="138"/>
+        <source>Report</source>
+        <translation>Raport</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="151"/>
+        <source>built-in v0.40</source>
+        <translation>wbudowana v0.40</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="161"/>
+        <source>built-in v0.37</source>
+        <translation>wbudiwana v0.37</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="171"/>
+        <source>custom</source>
+        <translation>własna</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="181"/>
+        <source>After successful clean-up:</source>
+        <translation>Po pomyślnym wyczyszczeniu:</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="190"/>
+        <source>Remove backups of plugins</source>
+        <translation>Usuń kopie zapasową pluginów</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="200"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Clean-up proccess create cache of master files in Data FIles folder.&lt;/p&gt;&lt;p&gt;It speed up cleaning a bit, but it takes some disk space as well.&lt;/p&gt;&lt;p&gt;When checkbox is checked, this cache is removed.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Proces czyszczenia tworzy cache plików master e folderze Data FIles.&lt;/p&gt;&lt;p&gt;To przyspiesza trochę czyszczeniaIt, ale zabiera również przestrzeń na dysku.&lt;/p&gt;&lt;p&gt;Gdy wybrane ten cache jest usuwany.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="203"/>
+        <source>Remove cache of master files</source>
+        <translation>Usuń cache plików master</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="234"/>
+        <source>Plugin</source>
+        <translation>Plugin</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="239"/>
+        <source>Status</source>
+        <translation>Status</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="244"/>
+        <source>Time</source>
+        <translation>Czas</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="252"/>
+        <source>Back</source>
+        <translation>Cofnij</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="277"/>
+        <source>File</source>
+        <translation>Plik</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="283"/>
+        <source>Help</source>
+        <translation>Pomoc</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="298"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Zakończ</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="301"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="306"/>
+        <source>Report issue</source>
+        <translation>Zgłoś problem</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="311"/>
+        <source>About Moht</source>
+        <translation>O Moht</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="316"/>
+        <source>About Qt</source>
+        <translation>O Qt</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="321"/>
+        <source>Check updates</source>
+        <translation>Splawdź aktualizację</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="187"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;During clean-up plugin file is copied to Data Files folder.&lt;/p&gt;&lt;p&gt;Cleaning proces create new plugin file.&lt;/p&gt;&lt;p&gt;When checkbox is checked dirty plugin file is removed.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Podczas czyszczenia plugin jest kopiowany do folderu Data Files.&lt;/p&gt;&lt;p&gt;Proces czyszczenia tworzy nowy plik pluginu.&lt;/p&gt;&lt;p&gt;Gdy wybrany brufny plugin jest usuwany.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtgui.ui" line="145"/>
+        <source>Version of tes3cmd:</source>
+        <translation>Wersja tes3cmd:</translation>
+    </message>
+</context>
+</TS>
